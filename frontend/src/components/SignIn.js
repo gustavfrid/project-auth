401c350e4124deca91fd10da12deb179beb75c2e
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-01-11 20:16:10 +0100

    style sign-in page

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,4 +1,60 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-top: 1px solid black;
+  max-width: 200px;
+  margin: 20px auto;
+`;
+
+const TextInput = styled.input`
+  position: relative;
+  display: inline-block;
+  margin: 10px;
+  padding: 5px 5px;
+  text-align: center;
+  font-family: inherit;
+  font-size: 12px;
+  text-decoration: none;
+  color: black;
+  background: rgba(218, 46, 46, 0);
+  transition: ease-out 0.5s;
+  border: 2px solid black;
+  height: 12px;
+
+  &:active {
+    transform: scale(0.9);
+  }
+`;
+
+const SubmitBtn = styled.input`
+  position: relative;
+  display: inline-block;
+  margin: 10px;
+  padding: 5px 5px;
+  text-align: center;
+  font-family: inherit;
+  font-size: 12px;
+  text-decoration: none;
+  color: black;
+  background: rgba(218, 46, 46, 0);
+  cursor: pointer;
+  transition: ease-out 0.5s;
+  border: 2px solid black;
+  height: 25px;
+
+  &:hover {
+    color: white;
+    box-shadow: inset 0 -100px 0 0 rgba(0, 0, 0, 0.6);
+  }
+
+  &:active {
+    transform: scale(0.9);
+  }
+`;
 
 const SignIn = (props) => {
   const { nameInput, passwordInput, setNameInput, setPasswordInput } =
@@ -6,7 +62,7 @@ const SignIn = (props) => {
   const onSignIn = (event) => {
     event.preventDefault();
 
-    fetch("http://localhost:8080/signin", {
+    fetch("https://technigo-project-auth.herokuapp.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -23,9 +79,10 @@ const SignIn = (props) => {
   };
 
   return (
-    <form onSubmit={onSignIn}>
+    <Container onSubmit={onSignIn}>
+      <h1>Sign in</h1>
       <label htmlFor="nameInput">Username</label>
-      <input
+      <TextInput
         id="nameInput"
         type="text"
         placeholder="username"
@@ -34,15 +91,15 @@ const SignIn = (props) => {
         }}
       />
       <label htmlFor="passwordInput">Password</label>
-      <input
+      <TextInput
         id="passwordInput"
         type="password"
         onChange={(e) => {
           setPasswordInput(e.target.value);
         }}
       />
-      <input type="submit" value="Sign in" />
-    </form>
+      <SubmitBtn type="submit" value="Sign in" />
+    </Container>
   );
 };
 
