ace376de060c30339d577c5a28e5ce772fc7e80f
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-01-11 10:48:07 +0100

    add styling for start-page

--- frontend/src/components/StartPage.js
+++ frontend/src/components/StartPage.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-top: 1px solid black;
+  max-width: 200px;
+  margin: 20px auto;
+`;
 
 const StartPage = () => {
   return (
-    <section>
+    <Container>
       <h1>Start</h1>
-    </section>
+    </Container>
   );
 };
 
