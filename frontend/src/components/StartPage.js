857f89554e19d237129bee325b8c1b93c7b2c1a5
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2022-01-11 19:47:10 +0100

    add buttons for signin and signup on startpage

--- frontend/src/components/StartPage.js
+++ frontend/src/components/StartPage.js
@@ -10,10 +10,44 @@ const Container = styled.section`
   margin: 20px auto;
 `;
 
+const Button = styled.div`
+  position: relative;
+  display: inline-block;
+  margin: 10px;
+  padding: 5px 5px;
+  text-align: center;
+  font-family: inherit;
+  font-size: 12px;
+  text-decoration: none;
+  color: black;
+  background: rgba(218, 46, 46, 0);
+  cursor: pointer;
+  transition: ease-out 0.5s;
+  border: 2px solid black;
+  height: 12px;
+
+  &:hover {
+    color: white;
+    box-shadow: inset 0 -100px 0 0 rgba(0, 0, 0, 0.6);
+  }
+
+  &:active {
+    transform: scale(0.9);
+  }
+`;
+
 const StartPage = () => {
+  const goToSignIn = () => {
+    console.log("sign in");
+  };
+  const goToSignUp = () => {
+    console.log("sign up");
+  };
   return (
     <Container>
-      <h1>Start</h1>
+      <h1>Startpage</h1>
+      <Button onClick={() => goToSignUp()}>Sign up</Button>
+      <Button onClick={() => goToSignIn()}>Sign in</Button>
     </Container>
   );
 };
